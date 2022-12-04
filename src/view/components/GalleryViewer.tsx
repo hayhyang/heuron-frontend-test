f5be36c18a5d74b651c65c1bd454c29e14d94cb5
hay <hayhyang@gmail.com>
2022-12-04 16:20:37 +0900

    feat: Add canvas wheel event

--- src/view/components/GalleryViewer.tsx
+++ src/view/components/GalleryViewer.tsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
 
 interface CanvasProps {
@@ -22,10 +23,25 @@ const GalleryViewer = ({ data, loading }: CanvasProps) => {
     };
   });
 
+  const navigate = useNavigate();
+  const onWheel = (event: any) => {
+    if (event.deltaY > 0) {
+      console.log('위로');
+      if (data.id < 0) return;
+      navigate(`/${Number(data.id) + 1}`);
+    }
+    if (event.deltaY < 0) {
+      console.log('아래로');
+      if (data.id > 29) return;
+      navigate(`/${Number(data.id) - 1}`);
+    }
+  };
+
   if (loading) return <div>loading</div>;
 
   return (
     <Canvas
+      onWheel={onWheel}
       ref={canvasRef}
       width={window.innerWidth}
       height={window.innerHeight}
